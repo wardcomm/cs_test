b300bbb4c8478b7e1112f06ff73c2a2bf3a031d1
J. Chad Ward <jchadward@gmail.com>
2023-11-18 18:30:44 -0500

    Update pythpn_test.py
      # create function encoding using hash library

--- pythpn_test.py
+++ pythpn_test.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 
 #create mapping for in memory object
 url_mapping = {}
+
+  # create function encoding using hash library
+def encode_url(url):
+    hash_object = hashlib.md5(url.encode())
+    return hash_object.hexdigest()[:6]

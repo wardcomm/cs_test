c57707f6226d66e58278df0dc21bd6e17021dc1f
J. Chad Ward <jchadward@gmail.com>
2023-11-18 19:03:42 -0500

    # create function for encode request module

--- pythpn_test.py
+++ pythpn_test.py
@@ -11,3 +11,19 @@ url_mapping = {}
 def encode_url(url):
     hash_object = hashlib.md5(url.encode())
     return hash_object.hexdigest()[:6]
+ 
+ # create function for encode request module
+@app.route('/encode', methods=['POST'])
+def encode():
+    data = request.get_json()
+
+    if 'url' not in data:
+        return jsonify({'error': 'Missing URL parameter'}), 400
+
+    original_url = data['url']
+    short_url = encode_url(original_url)
+
+    # Store in-memory
+    url_mapping[short_url] = original_url
+
+    return jsonify({'short_url': short_url}), 200

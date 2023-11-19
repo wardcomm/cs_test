9fe78c08b869ce24a7bc585d53d2d54fdda0f471
J. Chad Ward <jchadward@gmail.com>
2023-11-18 20:45:24 -0500

    adding **kwargs

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def hello(**kwargs):
     return "Hello, World!"
--- pythpn_test.py
+++ pythpn_test.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import 
 import hashlib
 import time
 

316bc73b2547a0c6c8eb592ca5c67b7f78592b36
J. Chad Ward <jchadward@gmail.com>
2023-11-18 19:14:29 -0500

    # Create function for decoding request module

--- pythpn_test.py
+++ pythpn_test.py
@@ -4,15 +4,15 @@ import time
 
 app = Flask(__name__)
 
-#create mapping for in memory object
+#Create mapping for in memory object
 url_mapping = {}
 
-  # create function encoding using hash library
+  # Create function encoding using hash library
 def encode_url(url):
     hash_object = hashlib.md5(url.encode())
     return hash_object.hexdigest()[:6]
  
- # create function for encode request module
+ # Create function for encode request module
 @app.route('/encode', methods=['POST'])
 def encode():
     data = request.get_json()
@@ -27,3 +27,14 @@ def encode():
     url_mapping[short_url] = original_url
 
     return jsonify({'short_url': short_url}), 200
+
+# Create function for decoding request module
+@app.route('/decode/<string:short_url>', methods=['GET'])
+def decode(short_url):
+    original_url = url_mapping.get(short_url)
+
+    if original_url is None:
+        return jsonify({'error': 'Short URL not found'}), 404
+
+    return jsonify({'original_url': original_url}), 200
+

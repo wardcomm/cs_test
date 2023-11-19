22b76c9070cec804a3ee546bb0442ecd117b24bc
J. Chad Ward <jchadward@gmail.com>
2023-11-19 01:11:20 -0500

    using chatgpt ai to get a different perspective

--- notes.md
+++ notes.md
@@ -30,7 +30,7 @@
 # which updated below
 # Successfully installed Flask-3.0.0 Jinja2-3.1.2 MarkupSafe-2.1.3 Werkzeug-3.0.1 blinker-1.7.0 # click-8.1.7 importlib-metadata-6.8.0 itsdangerous-2.1.2 zipp-3.17.0
 
-
+```
 export FLASK_APP=app.py
 
 
@@ -47,9 +47,9 @@ openhabian@openhabian:~/REPO/cs_test $ flask run
 WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
  * Running on http://127.0.0.1:5000
 Press CTRL+C to quit
+```
 
-
-
+```
 openhabian@openhabian:~ $ sudo curl -v http://localhost:5000
 *   Trying ::1:5000...
 * connect to ::1 port 5000 failed: Connection refused
@@ -69,8 +69,8 @@ openhabian@openhabian:~ $ sudo curl -v http://localhost:5000
 < Connection: close
 <
 * Closing connection 0
-
-
+```
+```
 openhabian@openhabian:~/REPO/cs_test $ export FLASK_APP=test
 openhabian@openhabian:~/REPO/cs_test $ flask run
  * Serving Flask app 'test'
@@ -80,7 +80,7 @@ WARNING: This is a development server. Do not use it in a production deployment.
 Press CTRL+C to quit
 127.0.0.1 - - [18/Nov/2023 22:29:17] "GET / HTTP/1.1" 404 -
 
-got responce from api test
+## got responce from api test
 openhabian@openhabian:~ $ sudo curl -v http://127.0.0.1:5000/api/user
 *   Trying 127.0.0.1:5000...
 * Connected to 127.0.0.1 (127.0.0.1) port 5000 (#0)
@@ -102,5 +102,5 @@ openhabian@openhabian:~ $ sudo curl -v http://127.0.0.1:5000/api/user
 ## Good site for Hashlib module
 https://www.askpython.com/python-modules/python-hashlib-module  
 
-
+```
 
---
+++ short.py
@@ -0,0 +1,69 @@
+# Import necessary libraries
+from flask import Flask, request, jsonify
+import hashlib
+
+app = Flask(__name__)
+
+# In-memory dictionary to store encoded URLs
+url_mapping = {}
+
+# Rate limiting variables
+request_count = 0
+last_request_time = 0
+
+# Function to encode a URL
+def encode_url(original_url):
+    # Using MD5 hash as a simple encoding algorithm
+    hash_object = hashlib.md5(original_url.encode())
+    encoded_url = hash_object.hexdigest()[:6]
+    return f'http://someapp/{encoded_url}'
+
+# Function to decode a URL
+def decode_url(encoded_url):
+    # Extracting the encoded part from the short URL
+    encoded_part = encoded_url.split('/')[-1]
+    # Reverse the encoding process (not guaranteed to be unique)
+    for original_url, short_code in url_mapping.items():
+        if short_code == encoded_part:
+            return original_url
+    return None
+
+# /encode endpoint to shorten a URL
+@app.route('/encode', methods=['POST'])
+def encode():
+    global request_count, last_request_time
+    # Rate limiting check
+    current_time = time.time()
+    if current_time - last_request_time < 0.5:
+        return jsonify({"error": "Rate limit exceeded. Try again later."}), 429
+
+    request_count += 1
+    last_request_time = current_time
+
+    data = request.get_json()
+    original_url = data.get('url')
+
+    if not original_url:
+        return jsonify({"error": "Missing 'url' parameter"}), 400
+
+    # Check if the URL has already been encoded
+    if original_url in url_mapping:
+        short_url = url_mapping[original_url]
+    else:
+        # Encode the URL and store it
+        short_url = encode_url(original_url)
+        url_mapping[original_url] = short_url
+
+    return jsonify({"short_url": short_url}), 200
+
+# /decode endpoint to retrieve the original URL
+@app.route('/decode/<path:encoded_url>', methods=['GET'])
+def decode(encoded_url):
+    original_url = decode_url(encoded_url)
+    if original_url:
+        return jsonify({"original_url": original_url}), 200
+    else:
+        return jsonify({"error": "Invalid short URL"}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
